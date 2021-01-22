76123a43e9f9765adac6cddf9085be8f8f66173c
smuktar <limodirectphilly@gmail.com>
2021-01-22 11:04:05 -0500

    svg width and height,margin set up

--- D3_data_journalism/js/app.js
+++ D3_data_journalism/js/app.js
@@ -1,1 +1,13 @@
-// @TODO: YOUR CODE HERE!
+var svgWidth = 960;
+var svgHeight = 700;
+
+var margin = {
+  top: 30,
+  right: 40,
+  bottom: 60,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
